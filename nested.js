6031c9521f501507418a7c61119a6dc6ebfa7f99
Geordan Gesink <georgegeordan@gmail.com>
2025-12-12 14:46:35 +0100

    rename logs

--- nested.js
+++ nested.js
@@ -3,12 +3,12 @@ const { SystemLog } = require('bare-logger')
 const Console = require('bare-console')
 global.console = new Console(new SystemLog())
 
-console.log('nested worklet logs')
+console.log('nested.js logs')
 
 const { childWriteFd, childReadFd } = Bare.Thread.self.data
 const writePipe = new Pipe(childWriteFd)
 const readPipe = new Pipe(childReadFd)
 
 readPipe.on('data', () => {
-    writePipe.write('nested worklet connected')
+    writePipe.write('nested.js has connected')
 })
--- worklet.js
+++ worklet.js
@@ -4,13 +4,13 @@ const RPC = require('bare-rpc')
 const Pipe = require('bare-pipe')
 const goodbye = require('graceful-goodbye')
 
-console.log('worklet logs')
+console.log('worklet.js logs')
 
 // setup rpc to frontend
 const rpc = new RPC(BareKit.IPC, (req, error) => {
   if (req.command === 0) {
     const request = rpc.request(0)
-    request.send('worklet.js connected')
+    request.send('worklet.js has connected')
   } 
 })
 
