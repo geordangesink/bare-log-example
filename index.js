632942386e17dc853b99fd0661a16c93a472603f
Geordan Gesink <georgegeordan@gmail.com>
2026-01-08 01:30:47 +0100

    move component

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 import { registerRootComponent } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Worklet } from 'react-native-bare-kit';
+import bundle from './worklet.bundle.js'
 
-import App from './App';
+export default function App() {
+  const startWorklet = () => {
+    const worklet = new Worklet()
+    worklet.start('/worklet.bundle',bundle)
+  }
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+  return (
+    <View style={styles.container}>
+      <Text>App is open</Text>
+      <Button title='Run Worklet' onPress={startWorklet} color='#b0d943' />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#444444ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 registerRootComponent(App);
