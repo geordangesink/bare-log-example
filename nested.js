075ae56d5ce84f499dc9bcd312fd34c37ece123c
Geordan Gesink <georgegeordan@gmail.com>
2026-01-09 13:21:34 +0100

    parentPort bug

--- nested.js
+++ nested.js
@@ -1,5 +1,7 @@
 const SystemLog = require('bare-system-logger')
 const Console = require('bare-console')
 global.console = new Console(new SystemLog())
+const Worker = require('bare-worker')
 
-console.log('this should log')
+console.log('parent Port is null:', Worker.parentPort === null)
+Worker.parentPort.postMessage('throws')
